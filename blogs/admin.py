7e93f447c047c4685990c79a5af01db6a691396d
Abuya Hassan <hassanabuya82@gmail.com>
2023-12-18 23:45:54 +0300

    admin panel

--- blogs/admin.py
+++ blogs/admin.py
@@ -7,4 +7,16 @@ from .models import *
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["id","created_by", "created_on", "image","content","title"]
+    list_display = ["id","title","created_by", "created_on", "image","content"]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["id","post","name", "email"]
+
+@admin.register(ContactUs)
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = ["id","name","email", "subject", "message"]
+
+@admin.register(ContactEmail)
+class ContactEmailAdmin(admin.ModelAdmin):
+    list_display = ["id","email"]
